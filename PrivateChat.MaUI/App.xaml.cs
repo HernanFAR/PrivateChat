c42f2c740c25fdedf7f46ae76f2c772ac5a0dabb
Hernan <h.f.alvarez.rubio>
2023-06-30 07:18:23 -0400

    FEAT: Avances en el proyecto de cliente

---
+++ PrivateChat.MaUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PrivateChat.MaUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ PrivateChat.MaUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PrivateChat.MaUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
